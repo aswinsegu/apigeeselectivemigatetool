a679842b6cb0616441a231118f45f648bf29fffb
neerajwadhwa <wadhwaneeraj.nw@gmail.com>
2017-07-15 19:21:29 +0530

    need to commit

---
+++ config.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Neeraj Wadhwa on 7/3/17.
+ */
+
+module.exports = {
+
+    from: {
+        version: '17.05',
+        url: 'https://api.enterprise.apigee.com/v1',
+        userid: 'wadhwaneeraj.nw@gmail.com',
+        passwd: 'Digitalapijourney123',
+        org: 'neerajwadhwaorganization',
+        env: 'test'
+    },
+    to: {
+        version: '17.05',
+        url: 'https://api.enterprise.apigee.com/v1',
+        userid: 'nw3360@gmail.com',
+        passwd: 'Digitalapijourney123',
+        org: 'nw3360-trial',
+        env: 'prod'
+    }
+} ;
---
+++ entityGetters/targetServers.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Neeraj Wadhwa on 7/3/17.
+ */
+
+
